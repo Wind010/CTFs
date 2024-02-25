7dc817503728a1b9aa7e56f0eaf6a41d132242ba
wind010 <wind010@users.noreply.github.com>
2024-02-25 12:16:44 -0800

    Add github action workflow and testing it

--- SSH_Server/ctf.sh
+++ SSH_Server/ctf.sh
@@ -17,22 +17,22 @@ timeout 3s cmatrix
 
 
 # Substring to check for
-substring="pass butter"
+substring=""
 
 while true; do
     # Prompt user for input
-    echo -e -n "${GREEN}What is my purpose?: ${NC}"
+    echo -e -n "${GREEN}Query?: ${NC}"
     read -r user_input
-    #read -r -p "What is my purpose?: " user_input
+    #read -r -p "Query: " user_input
 
     # Check if the entered string contains the substring
     if [[ "$user_input" == *"$substring"* ]]; then
         echo "OMG..."
-        echo -e "${CYAN}nUE0pUZ6Yl9xov5bqP9jnJAeoTIwLKDi${NC}\n\n"
+        echo -e "${CYAN}Nice!${NC}\n\n"
 
         # Don't use exit/break since this will kill the script and leave user in ssh terminal.
         pkill -u $(whoami) sshd
     else
-        echo -e "${RED}V3JvbmcgQW5zd2VyIQ==${NC}\n"
+        echo -e "${RED}Boo!${NC}\n"
     fi
 done
